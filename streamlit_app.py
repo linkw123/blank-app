46ddc1b5a67290ae210be32b044fbb9595249c57
linkw123 <17888649823@139.com>
2026-09-22 14:23:54 +0800

    Update Streamlit app title and configuration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
+import os
+os.environ["MPLBACKEND"] = "Agg"
+
+import pandas as pd
+import matplotlib
+matplotlib.use("Agg", force=True)
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="AI Cockpit Dashboard", layout="wide")
+st.title("AI Cockpit for a Manufacturing Line")
+st.markdown("Bringing together predictive maintenance and analytics.")
